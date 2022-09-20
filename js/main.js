3416d0425bc08721e6867f439b10a226f7e7518d
Kevin <klzheng@buffalo.edu>
2022-09-20 17:29:37 -0400

    Created and styled search page, search terms work

--- js/main.js
+++ js/main.js
@@ -1,69 +1,71 @@
 const API_KEY = "5b7ff1ca08f2367f1d77090c6730231d"
 const BASE_URL = "https://api.themoviedb.org/3"
-
 const PARAM = "&language=en-US&page=1"
 const API_URL_TRENDING = BASE_URL + "/trending/all/week?api_key=" + API_KEY
 const API_URL_TRENDING_MOV = BASE_URL + "/movie/popular?api_key=" + API_KEY + PARAM
 const API_URL_TRENDING_SHOW = BASE_URL + "/tv/popular?api_key=" + API_KEY + PARAM
 const API_URL_THEATERS = BASE_URL + "/movie/now_playing?api_key=" + API_KEY + PARAM
 const IMG_URL = "https://image.tmdb.org/t/p/w500"
-"/search/multi?api_key="
-// const query = "&query=the+boys"
+const API_SEARCH = "https://api.themoviedb.org/3/search/multi?&api_key=5b7ff1ca08f2367f1d77090c6730231d&language=en-US&page=1&query="
+BASE_URL + "/search/multi?&api_key=" + API_KEY + PARAM + "&query="
+
 
 // Gets API data from url with a specific target in HTML
-function getData(url, target) {
+function getData(url, target, cardStyle1, cardStyle2, cardStyle3, cardStyle4) {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data.results)
-            displayMovies(data.results, target)
+            console.log(data)
+            displayMovies(data.results, target, cardStyle1, cardStyle2, cardStyle3, cardStyle4)
         })
         .catch(err => console.log(err))
 }
 
 // Displays each film inside the data
-function displayMovies(data, target) {
+function displayMovies(data, target, cardStyle1, cardStyle2, cardStyle3, cardStyle4) {
     data.forEach(movie => {
-        const trendingCards = document.querySelector(target)
+        const targetLocation = document.querySelector(target)
         const movieCol = document.createElement("div")
-        let {name, title, overview, poster_path, vote_average} = movie
+        let {name, title, overview, poster_path, vote_average, release_date, first_air_date} = movie
         name = checkFilmName(name, title)
-        movieOverview = createCard(name, overview, poster_path, vote_average)
-        movieName = createCardName(name)
+        imagePath = checkImg(poster_path)
+        movieOverview = createCard(name, overview, imagePath, vote_average, release_date, cardStyle1, cardStyle2, cardStyle3, cardStyle4)
+        movieName = createCardName(name, cardStyle4)
         movieCol.appendChild(movieOverview)
         movieCol.appendChild(movieName)
-        trendingCards.appendChild(movieCol)
+        targetLocation.appendChild(movieCol)
     })
 }
 
 // Checks that the correct movie/show title is returned
 function checkFilmName(name, title) {
-    if (name === undefined) {
-        name = title
-    } 
-    return name
+    return name === undefined ? name = title : name
 }
 
 // Creates card with hover information
-function createCard(name, overview, poster_path, rating){
+function createCard(name, overview, image, rating, releaseDate, cardClass, cardInfo, cardRating, cardName){
     const movieOverview = document.createElement("div")
-    movieOverview.classList.add("card")
+    movieOverview.classList.add(cardClass)
     movieOverview.innerHTML = 
         `
-        <img src="${IMG_URL + poster_path}" alt="${name}">
-        <div class="card-info">
+        <img src="${image}" alt="${name}">
+        <div class="${cardInfo}">
+            <h3 class="${cardName}">
+                ${name}
+            </h3>
+            <p>${releaseDate}</p>
             <p>${overview}</p>
         </div>
-        <span class="card-rating">${rating.toFixed(1)}</span>
+        <span class="${cardRating}">${rating.toFixed(1)}</span>
         `
     return movieOverview
 }
 
 
 // Creates card name tag
-function createCardName(name) {
+function createCardName(name, cardName) {
     const movieName = document.createElement("div")
-    movieName.classList.add("card-name")
+    movieName.classList.add(cardName)
     movieName.innerHTML = 
         `
         <h3>${name}</h3>
@@ -71,25 +73,50 @@ function createCardName(name) {
     return movieName
 }
 
+// Manages popular options slider
 function changeTrending(el, classEl) {
     classEl.forEach(item => item.classList.remove("popular-slider-selected"))
     el.classList.toggle("popular-slider-selected")
     document.querySelector(".cards-trending").innerHTML = ""
     switch (el.id) {
         case "all": 
-            getData(API_URL_TRENDING, ".cards-trending");
+            getData(API_URL_TRENDING, ".cards-trending", "card", "card-info", "card-rating", "card-name");
         case "movies": 
-            getData(API_URL_TRENDING_MOV, ".cards-trending");
+            getData(API_URL_TRENDING_MOV, ".cards-trending", "card", "card-info", "card-rating", "card-name");
         case "shows":
-            getData(API_URL_TRENDING_SHOW, ".cards-trending");
+            getData(API_URL_TRENDING_SHOW, ".cards-trending", "card", "card-info", "card-rating", "card-name");
     }
 }
 
+// Checks image path for valid result, if not a default image path is returned
+function checkImg(imgPath) {
+    return imgPath ? `${IMG_URL + imgPath}` : "./images/default.jpg"
+}
+
+// resets form after submit
+function resetForm(e) {
+    e.target.reset()
+}
+
+// Creates slider options
 const sliderOptions = document.querySelectorAll(".popular-slider-option")
 sliderOptions.forEach(div => div.addEventListener("click", () => changeTrending(div, sliderOptions)))
 
+// Generates default results on homepage
+getData(API_URL_TRENDING, ".cards-trending", "card", "card-info", "card-rating", "card-name")
+getData(API_URL_THEATERS, ".cards-theater", "card", "card-info", "card-rating", "card-name")
 
-getData(API_URL_TRENDING, ".cards-trending")
-getData(API_URL_THEATERS, ".cards-theater")
+// Search movies db for results
+const form = document.querySelector("#main-search-form")
+const search = document.querySelector("#main-search")
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const query = search.value;
+    if (query) {
+        document.querySelector(".cards-search").innerHTML = ""
+        getData(API_SEARCH + query, ".cards-search", "card-search", "card-search-info", "card-search-rating", "card-search-name");
+        search.value = "";
+    }
+});
 
