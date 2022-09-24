5ec6d351d4e6302f7b4c3c7cf47887f85a809e77
Kevin <klzheng@buffalo.edu>
2022-09-23 20:08:16 -0400

    Refactored code

--- middleware/error.js
+++
@@ -1,8 +0,0 @@
-require("express-async-errors")
-
-// Handles error without stopping app
-const errorHandler = (error, req, res, next) => {
-    res.json({error: error.message || error})
-}
-
-module.exports = errorHandler
