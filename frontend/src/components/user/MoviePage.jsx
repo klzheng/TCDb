684a952eadf61a1514c75eae387f8e105fe9b630
Kevin <klzheng@buffalo.edu>
2022-10-15 00:03:08 -0400

    Implemented trailer to movie details page Added and formatted various details (title, ratings, cast, director, writers, release date, genres, languages)

--- frontend/src/components/user/MoviePage.jsx
+++ frontend/src/components/user/MoviePage.jsx
@@ -1,4 +1,7 @@
 import React from "react"
+import ReactPlayer from "react-player"
+import { AiFillStar } from "react-icons/ai"
+import moment from "moment"
 import { useEffect } from "react"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
@@ -7,6 +10,15 @@ import { useParams } from "react-router-dom"
 export default function MoviePage() {
 
     const [pageDetails, setPageDetails] = useState([])
+    const [trailerKey, setTrailerKey] = useState("")
+    const [credits, setCredits] = useState([])
+
+    const [writers, setWriters] = useState([])
+    const [director, setDirector] = useState([])
+    const [cast, setCast] = useState([])
+    const [languages, setLanguages] = useState([])
+    const [releaseDate, setReleaseDate] = useState([])
+    const [genres, setGenres] = useState([])
 
     const res = useParams()
     // console.log(res)
@@ -16,24 +28,92 @@ export default function MoviePage() {
     const grabData = async (url) => {
         const res = await fetch(url)
         const data = await res.json()
-        console.log(data.results.filter((item) => item.type === "Trailer"))
-        setPageDetails(data)
+        return data
     }
 
+
     useEffect(() => {
-        // grabData(`https://api.themoviedb.org/3/${res.mediaType}/${res.id}?api_key=5b7ff1ca08f2367f1d77090c6730231d&append_to_response=videos`)
+
         grabData(`https://api.themoviedb.org/3/${res.mediaType}/${res.id}/videos?api_key=5b7ff1ca08f2367f1d77090c6730231d`)
+            .then(data => setTrailerKey(data.results.filter((item) => item.type === "Trailer")[0].key))
+
+        grabData(`https://api.themoviedb.org/3/${res.mediaType}/${res.id}?api_key=5b7ff1ca08f2367f1d77090c6730231d`)
+            .then(data => {
+                setPageDetails(data)
+                setLanguages(data.spoken_languages.map(item => item.english_name))
+                setReleaseDate(moment(data.release_date).format('MMMM Do YYYY'))
+                setGenres(data.genres.map(item => item.name))
+            })
+
+        grabData(`https://api.themoviedb.org/3/${res.mediaType}/${res.id}/credits?api_key=5b7ff1ca08f2367f1d77090c6730231d`)
+            .then(data => {
+                setCredits(data)
+                console.log(data.crew.filter(item => item.known_for_department === "Writing").map(item => item.name).join(" "))
+                setDirector(data.crew.filter(item => item.known_for_department === "Directing")[0].name)
+                setWriters(data.crew.filter(item => item.known_for_department === "Writing").map(item => item.name))
+                setCast(data.cast)
+            })
+
     }, [res.id, res.mediaType])
+
+
+
+
 
 
 
     return (
         <div className="fixed inset-0 bg-gradient-to-b from-bg-start to-black -z-10 overflow-auto">
             <div className="mx-32 my-28 text-gray-400 flex-auto ">
-                <div>
-                    {/* {pageDetails.name} */}
-                    {/* {pageDetails} */}
+
+                <div className="player-wrapper border-2 border-gray-500 ">
+                    <ReactPlayer url={`youtube.com/watch?v=${trailerKey}`} controls={true} pip={true} width="100%" height="100%" className="react-player" />
                 </div>
+                <div className="flex-col space-y-4">
+                    <p className="text-4xl font-semibold my-8 text-white flex justify-between">
+                        {pageDetails.title || pageDetails.name}
+                        <span className=" border-2 border-white rounded pl-2 flex items-center">
+                            {pageDetails.vote_average && pageDetails.vote_average.toFixed(1)}
+                            <AiFillStar className="" />
+                        </span>
+                    </p>
+                    <p>{pageDetails.overview}</p>
+                    <p className="text-xl ">
+                        Cast
+                    </p>
+                    <p className="flex space-x-4 overflow-auto">
+                        {cast.map((person) => (
+                            <div className="text-center">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
+                                    alt="Cast Member"
+                                    className="max-w-xs h-60 object-cover rounded" />
+                                <p className="font-bold text-gray-200">
+                                    {person.character}
+                                </p>
+                                <p className="text-base">
+                                    {person.name}
+                                </p>
+                            </div>
+                        ))}
+                    </p>
+
+                    <div><p className="drop-shadow-white-text text-gray-200">Director:</p> <span className="mx-3">{director}</span></div>
+                    <div><p className="drop-shadow-white-text text-gray-200">Writers:</p> 
+                        {writers.map((writer) => (
+                            <span className="px-3">{writer}</span>
+                        ))}
+                    </div>
+                    <div><p className="drop-shadow-white-text text-gray-200">Release Date:</p> <span className="px-3">{releaseDate}</span></div>
+                    <div><p className="drop-shadow-white-text text-gray-200">Genres:</p> 
+                        {genres && genres.map((genre) => (
+                            <span className="px-3">{genre}</span>
+                        ))}
+                    </div>
+                    <div><p className="drop-shadow-white-text text-gray-200">Languages:</p> <span className="px-3">{languages}</span></div>
+                </div>
+
+
             </div>
         </div>
     )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,19 +26,35 @@ module.exports = {
         40: ".40",
       },
       boxShadow: {
+        'white': '0 0 5px rgba(255, 255, 255, .5)',
         'whitexl': '0 0 25px rgba(255, 255, 255, .4)',
         "teal": "0 0 10px rgb(0, 128, 128)",
         "blur": "0px 100px 1000px 50px rgba(0, 0, 0, 0.6)",
       },
       dropShadow: {
         'white': '0 0 50px rgba(255, 255, 255, .8)',
+        'white-text': '0 0 2px rgba(255, 255, 255, .5)',
       },
       maxHeight: {
         '112': '28rem',
         '128': '32rem',
         '144': '36rem',
         '160': '40rem',
-      }
+      },    
+      spacing: {
+        '10%': '10%', 
+        '20%': '20%', 
+        '30%': '30%', 
+        '40%': '40%', 
+        '50%': '50%', 
+        '60%': '60%', 
+        '70%': '70%', 
+        '80%': '80%', 
+        '90%': '90%', 
+      },
+      backgroundImage: {
+        "fading" : "linear-gradient(90deg, transparent 95%, grey)",
+      },
     },
   },
   plugins: [
