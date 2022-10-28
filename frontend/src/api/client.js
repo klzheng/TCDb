0beb94dfdb20a047e7f2bf8006d6fe2a29dd887f
Kevin <klzheng@buffalo.edu>
2022-10-28 10:34:58 -0400

    moved base url to .env

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // uses axios to send http requests to endpoints
 const client = axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "https://seal-app-odxdb.ondigitalocean.app/api"
+  // baseURL: "https://seal-app-odxdb.ondigitalocean.app/api"
+  baseURL: process.env.REACT_APP_BASE_URL
 });
 
 export default client;
