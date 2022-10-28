1179bb0f2681817e0178d0522cd60cb4bdc43c8c
Kevin <klzheng@buffalo.edu>
2022-10-28 10:20:14 -0400

    changed port

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,8 @@ app.use("/*", handleNotFound);
 app.use(errorHandler);
 
 
-app.listen(8000, () => {
-  console.log("the port is listening on port 8000");
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log("the port is listening on port " + PORT);
 });
