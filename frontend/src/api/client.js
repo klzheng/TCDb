d004f46096757f00a460475530123b561ad8c3ca
Kevin <klzheng@buffalo.edu>
2022-10-27 22:56:23 -0400

    changed base url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 // uses axios to send http requests to endpoints
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://seal-app-odxdb.ondigitalocean.app/api"
 });
 
 export default client;
