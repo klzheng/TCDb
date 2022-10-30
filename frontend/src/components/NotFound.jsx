0698e2643fa25848d145cc63a190ac21f37a19bf
Kevin <klzheng@buffalo.edu>
2022-10-30 19:15:28 -0400

    Edited Not Found page to provide redirect link

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Background from "./Background";
 import Container from "./Container";
 
@@ -7,8 +8,11 @@ export default function NotFound() {
     return (
         <Background>
             <Container>
-                <div className="flex justify-center items-center">
-                    Page not found
+                <div className="flex flex-col justify-center items-center ">
+                    <p className="text-5xl py-10">Page not found</p>
+                    <p className="text-xl">Please go to the homepage&nbsp;  
+                        <Link to="/" className="underline text-gray-200">here</Link>
+                    </p>
                 </div>
             </Container>
         </Background>
